fc14d95b62a438769883965780e8635f509122ad
Thorsten Specht <thorsten.specht@aoe.com>
2020-06-15 22:39:44 +0200

    improve reaction test game with score and highscore

--- src/js/reaktionszeittester.js
+++ src/js/reaktionszeittester.js
@@ -6,13 +6,15 @@ document.getElementById("startButton").addEventListener("click", () => {
     let timeOfAppearance;
     let timeOfReaction;
     let timeToReact;
+    let highscore = 0;
 
+    /* Creates a random timeout between 0 and 2 seconds */
     let calculateTimeout = () => {
         let randomNumber = Math.random();
         timeout = randomNumber * 2000;
-        console.log(timeout);
-    }
+    };
 
+    /* Makes the target visible after the previously calculated timeout and starts the game */
     let displayTarget = () => {
         timeOfAppearance = Date.now();
         generateRandomForm();
@@ -20,19 +22,19 @@ document.getElementById("startButton").addEventListener("click", () => {
         setTimeout(function() {
             target.style.display = "inline-block";
         }, timeout, false);
-    }
+    };
 
+    /* Generates a form with random color, size and form at a random position */
     let generateRandomForm = () => {
         // Position
         let randomPositionX = Math.random() * 85;
-        let randomPositionY = Math.random() * 80;
+        let randomPositionY = Math.random() * 75;
         target.style.bottom = randomPositionY + "%";
         target.style.right = randomPositionX + "%";
 
         // Color
         let randomCase = Math.round((Math.random() * 3));
         let randomColor;
-        console.log(randomCase);
         switch (randomCase) {
             case 1:
                 randomColor = "#A22C29";
@@ -46,7 +48,7 @@ document.getElementById("startButton").addEventListener("click", () => {
             default:
                 randomColor = "#000";
                 break;
-        }
+        };
         target.style.backgroundColor = randomColor;
 
         // Size
@@ -63,19 +65,27 @@ document.getElementById("startButton").addEventListener("click", () => {
             randomBorderRadius = 0;
         }
         target.style.borderRadius = randomBorderRadius + "%";
-    }
+    };
 
+    /* Execute displayTarget function to start the game */
     displayTarget();
+    /* Listen for clicks on the target to make the game playable */
     target.addEventListener("click", function() {
         timeOfReaction = Date.now();
         timeToReact = timeOfReaction - timeOfAppearance;
         target.style.display = "none";
-        console.log(timeToReact);
+        document.getElementById("result").innerHTML = Math.round((timeToReact / 1000) * 100) / 100 + " s";
+        if (highscore == 0 || highscore > timeToReact) {
+            highscore = timeToReact;
+            document.getElementById("record").innerHTML = Math.round((highscore / 1000) * 100) / 100 + " s";
+        };
+        /* Call the displayTarget function inside the main function to automatically start the next round after the user clicked on the target */
         displayTarget();
-    })
+    });
 
-    // Exit game
+    /* Exit game */
     document.getElementById("stopButton").addEventListener("click", () => {
         target.style.display = "none";
-    })
+        document.getElementById("result").innerHTML = "";
+    });
 });
