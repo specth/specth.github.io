40c66d3751b8aa12137447313aef9368a68d6c2e
Thorsten Specht <thorsten.specht@aoe.com>
2020-06-19 15:02:17 +0200

    add blog entry 'meine karotte' and make image size on blog overview dynamic

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,15 +1,9 @@
-window.addEventListener("load", () => {
-    let blog__posts = document.getElementsByClassName("shortDescription");
+let resizeBlogPostImage = () => {
+    let blog__entries = document.getElementsByClassName("blogPreview");
+    let blog__images = document.getElementsByClassName("blogImage");
+    let imagesHeight = [];
 
-    for (post of blog__posts) {
-        post.innerText = post.innerText.substr(0, 200) + "\u2026";
-    }
-
-    /*  
-        let blog__entries = document.getElementsByClassName("blogPreview");
-        let blog__images = document.getElementsByClassName("blogImage");
-
-        let imagesHeight = [];
+    if (window.innerWidth > 768) {
         for (entry of blog__entries) {
             let height = entry.clientHeight - 50;
             imagesHeight.push(height)
@@ -17,11 +11,31 @@ window.addEventListener("load", () => {
 
         for (let i = 0; i < imagesHeight.length; i++) {
             blog__images[i].style.height = imagesHeight[i] + "px";
-            console.log(blog__images[i]);
-            console.log(imagesHeight[i]);
-            console.log(document.getElementsByClassName("blogImage")[i].clientHeight);
         }
-    */
+    } else if (blog__images[0].clientHeight != 150) {
+        for (image of blog__images) {
+            image.style.height = "150px";
+        }
+    }
+}
 
+let createBlogPostShortDescription = () => {
+    let blog__posts = document.getElementsByClassName("shortDescription");
+    let blog__links = document.querySelectorAll("div h2 a");
 
+    for (let i = 0; i < blog__posts.length; i++) {
+        link = blog__links[i].getAttribute("href");
+        shortDescription = blog__posts[i].innerText.substr(0, 245);
+
+        blog__posts[i].innerHTML = shortDescription + "\u2026 <a href='" + link + "'>[mehr]</a>";
+    }
+}
+
+window.addEventListener("load", () => {
+    createBlogPostShortDescription();
+    resizeBlogPostImage();
+
+    window.addEventListener("resize", () => {
+        resizeBlogPostImage();
+    });
 });
