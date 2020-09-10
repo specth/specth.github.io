df0a5ade374b91659021af5c1ccfd46318e218c2
DESKTOP-AHT2QLK\spech <specht.thorsten@googlemail.com>
2020-09-11 00:37:51 +0200

    Build reusable components

---
+++ src/js/components.js
@@ -0,0 +1,56 @@
+class VanillaFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML =
+        `
+        <!-- <footer id="footer">
+        <div id="footer-desktop" class="clearfix"></div>
+        </footer> -->
+        `;
+    }
+}
+
+class VanillaHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML =
+        `
+        <header id="header">
+        <nav>
+            <div id="desktop-nav">
+                <div class="topnav">
+                    <a href="index.html" class="topnav-left" id="desktop-nav-about">About</a>
+                    <a href="javascript.html" class="topnav-left" id="desktop-nav-vanilla">Vanilla JS</a>
+                    <a href="blog.html" class="topnav-left" id="desktop-nav-blog">Blog</a>
+                    <a href="impressum.html" class="topnav-left" id="desktop-nav-impressum">Impressum</a>                    
+                    <a href="#" class="fa fa-xing topnav-right"></a>
+                    <a href="#" class="fa fa-github topnav-right"></a>
+                </div>
+            </div>
+
+            <div id="mobile-nav">
+
+                <!-- sidebar navigation -->
+                <div id="mySidenav" class="sidenav">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                    <a href="index.html">About</a>
+                    <a href="javascript.html">Pure JS</a>
+                    <a href="blog.html">Blog</a>
+                    <a href="impressum.html">Impressum</a>    
+                    
+                    <div class="sidenav-social">
+                        <a href="#" class="fa fa-xing"></a>
+                        <a href="#" class="fa fa-github"></a>
+                    </div>
+                </div>                
+
+                <!-- Use any element to open the sidenav -->
+                <span id="openMenuBtn" onclick="openNav()">&equiv;</span>
+            </div>
+            <div id="header-vanilla"></div>
+        </nav>
+    </header>
+    `
+    }
+}
+
+customElements.define('vanilla-footer', VanillaFooter);
+customElements.define('vanilla-header', VanillaHeader);
--- src/js/nav.js
+++ src/js/nav.js
@@ -5,3 +5,27 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function hightlightCurrentPage() {
+    switch (window.location.pathname) {
+        case ("/index.html"):
+            document.getElementById("desktop-nav-about").classList.add("active");
+            break;
+        case ("/javascript.html"):
+            document.getElementById("desktop-nav-vanilla").classList.add("active");
+            break;
+        case ("/blog.html"):
+            document.getElementById("desktop-nav-blog").classList.add("active");
+            break;
+        case ("/impressum.html"):
+            document.getElementById("desktop-nav-impressum").classList.add("active");
+            break;
+        default:
+            break;
+    }
+    console.log(window.location.pathname);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    hightlightCurrentPage();
+});
