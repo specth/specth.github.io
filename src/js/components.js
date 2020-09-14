e867eb408a8473d6f5ba961efc879d582e47d792
Thorsten Specht <>
2020-09-14 22:06:56 +0200

    Add social profile links

--- src/js/components.js
+++ src/js/components.js
@@ -37,8 +37,8 @@ class VanillaHeader extends HTMLElement {
                     <a href="javascript.html" class="topnav-left" id="desktop-nav-vanilla">Vanilla JS</a>
                     <a href="blog.html" class="topnav-left" id="desktop-nav-blog">Blog</a>
                     <a href="impressum.html" class="topnav-left" id="desktop-nav-impressum">Impressum</a>                    
-                    <a href="#" class="fab fa-xing topnav-right"></a>
-                    <a href="#" class="fab fa-github topnav-right"></a>
+                    <a href="https://www.xing.com/profile/Thorsten_Specht6/cv" class="fab fa-xing topnav-right"></a>
+                    <a href="https://github.com/specthor" class="fab fa-github topnav-right"></a>
                 </div>
             </div>
 
@@ -48,13 +48,13 @@ class VanillaHeader extends HTMLElement {
                 <div id="mySidenav" class="sidenav">
                     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
                     <a href="index.html">About</a>
-                    <a href="javascript.html">Pure JS</a>
+                    <a href="javascript.html">Vanilla JS</a>
                     <a href="blog.html">Blog</a>
                     <a href="impressum.html">Impressum</a>    
                     
                     <div class="sidenav-social">
-                        <a href="#" class="fab fa-xing"></a>
-                        <a href="#" class="fab fa-github"></a>
+                        <a href="https://www.xing.com/profile/Thorsten_Specht6/cv" class="fab fa-xing"></a>
+                        <a href="https://github.com/specthor" class="fab fa-github"></a>
                     </div>
                 </div>                
 
