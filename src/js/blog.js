ee762124b9e827e4d429a7395ba9b3949467680a
Thorsten Specht <>
2020-09-17 07:13:26 +0200

    [WIP] Rework blog design

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,24 +1,3 @@
-let resizeBlogPostImage = () => {
-    let blog__entries = document.getElementsByClassName("blogPreview");
-    let blog__images = document.getElementsByClassName("blogImage");
-    let imagesHeight = [];
-
-    if (window.innerWidth > 768) {
-        for (entry of blog__entries) {
-            let height = entry.clientHeight - 50;
-            imagesHeight.push(height)
-        }
-
-        for (let i = 0; i < imagesHeight.length; i++) {
-            blog__images[i].style.height = imagesHeight[i] + "px";
-        }
-    } else if (blog__images[0].clientHeight != 150) {
-        for (image of blog__images) {
-            image.style.height = "150px";
-        }
-    }
-}
-
 let createBlogPostShortDescription = () => {
     let blog__posts = document.getElementsByClassName("shortDescription");
     let blog__links = document.querySelectorAll("div h2 a");
@@ -33,9 +12,4 @@ let createBlogPostShortDescription = () => {
 
 window.addEventListener("load", () => {
     createBlogPostShortDescription();
-    resizeBlogPostImage();
-
-    window.addEventListener("resize", () => {
-        resizeBlogPostImage();
-    });
 });
