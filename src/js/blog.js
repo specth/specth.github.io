e1d8b83588c822fd92530f1aee7d94a6bdd7fa64
DESKTOP-AHT2QLK\spech <specht.thorsten@googlemail.com>
2020-09-20 20:00:24 +0200

    Align height of blog entries

--- src/js/blog.js
+++ src/js/blog.js
@@ -10,6 +10,37 @@ let createBlogPostShortDescription = () => {
     }
 }
 
+let alignColumnHeight = () => {
+    let blog__rows = document.getElementsByClassName("row");
+    console.log(blog__rows)
+
+    // substract number of rows by 1 to exclude footer row
+    for(let i = 0; i < blog__rows.length - 1; i++) {
+        let rowHeight = blog__rows[i].clientHeight - 60;
+        let rowColumns = blog__rows[i].querySelectorAll(".shadow")
+
+        for(let i = 0; i < rowColumns.length; i++) {
+            console.log(rowColumns[i]);
+            rowColumns[i].style.height = rowHeight + "px";
+        }
+    }
+}
+
+window.addEventListener("load", () => {
+    alignColumnHeight();
+});
+
+// Ugly method to resize when browser width changes.
+var resizeId;
+window.addEventListener('resize', function() {
+    clearTimeout(resizeId);
+    resizeId = setTimeout(doneResizing, 100);
+});
+
+function doneResizing(){
+    location.reload();
+}
+
 window.addEventListener("load", () => {
     createBlogPostShortDescription();
 });
