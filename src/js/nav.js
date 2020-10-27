ad9fb9709e48315d5fe4506073ffb3a4022c284f
thorsten-specht <specht.thorsten@googlemail.com>
2020-10-27 22:40:16 +0100

    Delete unused nav function

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,30 +5,3 @@ let openNav = function() {
 let closeNav = function() {
     document.getElementById("mySidenav").style.width = "0";
 }
-
-let hightlightCurrentPage = function() {
-    switch (window.location.pathname) {
-        case ("/"):
-            document.getElementById("desktop-nav-about").classList.add("active");
-            break;
-        case ("index.html"):
-            document.getElementById("desktop-nav-about").classList.add("active");
-            break;
-        case ("javascript.html"):
-            document.getElementById("desktop-nav-vanilla").classList.add("active");
-            break;
-        case ("/blog.html"):
-            document.getElementById("desktop-nav-blog").classList.add("active");
-            break;
-        case ("impressum.html"):
-            document.getElementById("desktop-nav-impressum").classList.add("active");
-            break;
-        default:
-            document.getElementById("desktop-nav-blog").classList.add("active");
-            break;
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    hightlightCurrentPage();
-});
